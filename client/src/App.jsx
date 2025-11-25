a7cf37b73ef281cf05459703fe065dd8040a349d
harishihan-c <harishihan@outlook.com>
2025-11-25 15:01:30 +0530

    feat: Add react router dom to all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Bot from "./pages/Bot";
+import Player from "./pages/Player";
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/bot" element={<Bot />} />
+        <Route path="/player" element={<Player />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
