ea9715280e2ca3681aafe365169cd321280b1187
harishihan-c <harishihan@outlook.com>
2025-12-16 21:53:35 +0530

    feat: add borad image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import { useNavigate } from "react-router-dom";
+import assets from "../assets/assets";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -14,12 +15,13 @@ const Home = () => {
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen ">
       <NavBar />
       <div className="flex flex-col flex-1 justify-center items-center gap-10 font-black text-2xl">
-        <button onClick={handleClickBot} className="w-80 py-5 rounded-xl bg-[#479979] text-white">Play with Bot</button>
-        <button onClick={handleClickPlayer} className="w-80 py-5 rounded-xl border-[#479979] border-2 text-[#479979]">Play with Player</button>
+        <button onClick={handleClickBot} className="w-80 py-5 rounded-xl bg-[#479979] text-white cursor-pointer">Play with Bot</button>
+        <button onClick={handleClickPlayer} className="w-80 py-5 rounded-xl border-[#479979] border-2 text-[#479979] cursor-pointer">Play with Player</button>
       </div>
+      <img src={assets.board} alt="" className="absolute right-0 bottom-0 w-80"/>
     </div>
   );
 };
